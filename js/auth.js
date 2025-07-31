466d9a6cfb4d2c2feb315a02e6b6836067402a40
Lizeth-JA <joseangeleslizeth@gmail.com>
2025-07-31 17:39:55 -0600

    1versel

---
+++ js/auth.js
@@ -0,0 +1,11 @@
+function showRegister() {
+  document.getElementById("register").style.display = "block";
+}
+
+function login() {
+  alert("Aquí irá la lógica de login con Supabase.");
+}
+
+function register() {
+  alert("Aquí irá la lógica de registro con Supabase.");
+}
