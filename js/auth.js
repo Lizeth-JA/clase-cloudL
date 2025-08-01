77c35910a011e93915370d1182a8d3c8aa1d09a8
Lizeth-JA <joseangeleslizeth@gmail.com>
2025-07-31 21:00:04 -0600

    au5

--- js/auth.js
+++ js/auth.js
@@ -1,28 +1,21 @@
 const SUPABASE_URL = "https://qdapojtpjolrhjuutedx.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFkYXBvanRwam9scmhqdXV0ZWR4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQwMTM2MjIsImV4cCI6MjA2OTU4OTYyMn0.AgwEY0EXaWB_FZF7R4J9HLkym47Rvcm9bv15RyUTG20";
 
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
+const client = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
 function toggleForms() {
   const loginForm = document.getElementById("login-form");
   const registerForm = document.getElementById("register-form");
 
-  if (loginForm.style.display === "none") {
-    loginForm.style.display = "block";
-    registerForm.style.display = "none";
-  } else {
-    loginForm.style.display = "none";
-    registerForm.style.display = "block";
-  }
+  loginForm.style.display = loginForm.style.display === "none" ? "block" : "none";
+  registerForm.style.display = registerForm.style.display === "none" ? "block" : "none";
 }
-
 
 async function register() {
   const email = document.getElementById("reg-email").value;
   const password = document.getElementById("reg-password").value;
 
-  const { data, error } = await supabase.auth.signUp({
+  const { data, error } = await client.auth.signUp({
     email,
     password,
   });
@@ -31,16 +24,15 @@ async function register() {
     alert("❌ Error: " + error.message);
   } else {
     alert("✅ Registro exitoso. Revisa tu correo.");
-    toggleForms(); 
+    toggleForms();
   }
 }
-
 
 async function login() {
   const email = document.getElementById("email").value;
   const password = document.getElementById("password").value;
 
-  const { data, error } = await supabase.auth.signInWithPassword({
+  const { data, error } = await client.auth.signInWithPassword({
     email,
     password,
   });
@@ -50,6 +42,5 @@ async function login() {
   } else {
     alert("✅ Sesión iniciada.");
     localStorage.setItem("token", data.session.access_token);
-    
   }
 }
